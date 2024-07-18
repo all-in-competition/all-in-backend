85b839586e4c380f62e4cf367f37acdb35b2ae93
Algorithm <fighter0628@gmail.com>
2024-07-18 17:43:57 +0900

    feat: 처음 로그인 확인하는 로직 및 카카오에서 사용자 정보 가져오는 로직 추가
    - 사용자 DB에 추가하는 로직 수정
    - 처음 로그인 확인하는 로직 추가
    - 사용자 정보 가져오는 로직 추가

--- api/cruds/member.py
+++ api/cruds/member.py
@@ -1,13 +1,34 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 import api.models.model as member_model
-from api.schemas.login import SessionData
+from api.schemas import login as login_schema
+from aiohttp import ClientSession
+
+def create_member(db: Session, member_create: login_schema.SessionData):
+    new_member = member_model.Member(**member_create.model_dump())
+    db.add(new_member)
+    db.commit()
 
 
-def create_member(db: Session, member_create: SessionData):
-    db.add(member_model.Member(**member_create.model_dump()))
-
-
-def find_member(db: Session, user_info: SessionData):
+def find_member(db: Session, user_info: login_schema.SessionData):
     return db.query(member_model.Member).filter(and_(member_model.Member.provider_type == user_info.provider_type,
                                                      member_model.Member.provider_id == user_info.provider_id))
+
+
+def is_first_login(db: Session, session_data: login_schema.SessionData) -> bool:
+    find_user = find_member(db=db, user_info=session_data)
+    if find_user:
+        return True
+    else:
+        return False
+
+
+async def get_kakao_user_info(token):
+    user_info_url = "https://kapi.kakao.com/v2/user/me"
+    headers = {"Authorization": f"Bearer {token}"}
+
+    async with ClientSession() as client:
+        async with client.get(user_info_url, headers=headers) as response:
+            if response.status != 200:
+                raise HTTPException(status_code=response.status, detail="Failed to fetch user info from Kakao")
+            return await response.json()
