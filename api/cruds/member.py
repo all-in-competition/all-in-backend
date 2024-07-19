0eb164d37b8154e473d5b370405773b577b95f6e
KJH <fighter0628@gmail.com>
2024-07-19 22:21:33 +0900

    feat: 메서드 find_member 수정 및 is_first_login 삭제
    - find_member에서 출력을 member_schema.MemberCreate로 변환해서 반환

--- api/cruds/member.py
+++ api/cruds/member.py
@@ -2,25 +2,35 @@ from sqlalchemy.orm import Session
 from sqlalchemy import and_
 import api.models.model as member_model
 from api.schemas import login as login_schema
+from api.schemas import member as member_schema
 from aiohttp import ClientSession
 
-def create_member(db: Session, member_create: login_schema.SessionData):
+def create_member(db: Session, member_create: login_schema.SessionData) -> member_schema.MemberCreate:
     new_member = member_model.Member(**member_create.model_dump())
     db.add(new_member)
     db.commit()
+    db.refresh(new_member)
+
+    return member_schema.MemberCreate(
+        id = new_member.id,
+        provider_id = new_member.provider_id,
+        provider_type = new_member.provider_type,
+        nickname = new_member.nickname,
+        status = new_member.status
+    )
 
 
-def find_member(db: Session, user_info: login_schema.SessionData):
-    return db.query(member_model.Member).filter(and_(member_model.Member.provider_type == user_info.provider_type,
-                                                     member_model.Member.provider_id == user_info.provider_id))
+def find_member(db: Session, user_info) -> member_model.Member:
+    db_member = db.query(member_model.Member).filter(and_(member_model.Member.provider_type == "kakao",
+                                                     member_model.Member.provider_id == user_info['id'])).first()
 
-
-def is_first_login(db: Session, session_data: login_schema.SessionData) -> bool:
-    find_user = find_member(db=db, user_info=session_data)
-    if find_user:
-        return True
-    else:
-        return False
+    return member_schema.MemberCreate(
+        id = db_member.id,
+        provider_id = db_member.provider_id,
+        provider_type = db_member.provider_type,
+        nickname = db_member.nickname,
+        status = db_member.status
+    )
 
 
 async def get_kakao_user_info(token):
@@ -31,4 +41,6 @@ async def get_kakao_user_info(token):
         async with client.get(user_info_url, headers=headers) as response:
             if response.status != 200:
                 raise HTTPException(status_code=response.status, detail="Failed to fetch user info from Kakao")
-            return await response.json()
+            user_info = await response.json()
+            print(user_info)
+            return user_info
