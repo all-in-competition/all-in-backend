cf008291fad7c118cb1d54a7f7ee4454cfee7a17
KJH <fighter0628@gmail.com>
2024-07-19 18:06:29 +0900

    chore: 사용하지 요청 처리하는 함수 제거

--- api/main.py
+++ api/main.py
@@ -10,9 +10,5 @@ app.include_router(post.router)
 
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 
-@app.get("/hello")
-async def hello():
-  return {"message": "hello world!"}
-
 if __name__ == "__main__":
   uvicorn.run(app, host="127.0.0.1", port=8000)
