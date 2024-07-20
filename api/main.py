caacc4872b65b98c2c7b9a37916a362cc130388b
KJH <fighter0628@gmail.com>
2024-07-21 00:46:47 +0900

    feat: 페이지네이션 기능 추가
    - fastapi_pagination 라이브러리를 사용하여 페이지네이션 기능 추가

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 from api.routers import login, post, resume
-from sqlalchemy.sql.functions import user
 from starlette.middleware.sessions import SessionMiddleware
 from api.configs.app_config import settings
+from fastapi_pagination import add_pagination
 
 app = FastAPI(root_path="/api")
 app.include_router(login.router)
@@ -11,6 +11,8 @@ app.include_router(post.router)
 app.include_router(resume.router)
 
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
+
+add_pagination(app)
 
 if __name__ == "__main__":
   uvicorn.run(app, host="127.0.0.1", port=8000)
