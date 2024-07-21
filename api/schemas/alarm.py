2c162e728a6c3b797eb9c402409d3a82fd483f6d
zxnm0682 <zxnm0682@gmail.com>
2024-07-22 05:23:53 +0900

    feat 알람 생성, 목록 조회, 상세 조회 기능 구현

--- api/schemas/alarm.py
+++ api/schemas/alarm.py
@@ -1,8 +1,24 @@
+from datetime import datetime
+
 from pydantic import BaseModel, Field
 
 
 class AlarmCreate(BaseModel):
-  sender_id : int
-  receiver_id : int
-  post_id : int
-  type : int
+      sender_id : int
+      receiver_id : int
+      post_id : int
+      #type : int
+
+class AlarmSummaryResponse(BaseModel):
+    sender_name : str
+    post_title : str
+    post_id : int
+    alarm_id : int
+
+class AlarmDetailResponse(BaseModel):
+    sender_id : int
+    sender_name : str
+    reciver_name : str
+    post_title : str
+    post_id : int
+    create_at : datetime
