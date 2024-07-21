d5fb205135aecedf272f7bc027e3671f800f5a62
zxnm0682 <zxnm0682@gmail.com>
2024-07-21 17:06:08 +0900

    fix ResumeUpdate와 ResumeCreate의 변수 수정

--- api/schemas/resume.py
+++ api/schemas/resume.py
@@ -6,13 +6,13 @@ from api.models.model import Tag
 class ResumeUpdate(BaseModel):
   contents : str
   public : bool = False
+  category_id : int
+  tag_name : List[str]
 
 class ResumeCreate(BaseModel):
   member_id : int
   contents : str
   public : bool
-  category_id : int
-  tag_name : List[str]
 
 class ResumeResponse(BaseModel):
   member_name : str
