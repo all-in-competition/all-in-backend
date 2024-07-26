4792021d36d29c13d2f26b590b1454006b1fadf7
Algorithm <fighter0628@gmail.com>
2024-07-26 15:30:39 +0900

    feat: get_private_chatroom, get_public_chatroom 함수 구현
    - 단체, 개인 채팅방 조회하는 로직 구현

--- api/cruds/chatroom.py
+++ api/cruds/chatroom.py
@@ -1,7 +1,9 @@
-from typing import Optional
+from typing import Optional, List
 
+from api.schemas.chatroom import PrivateChatroom, PublicChatroom
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from api.models.model import Chatroom, member_chatroom, Member
+from api.models.model import Chatroom, member_chatroom, Member, Post
 
 
 def get_chatroom(db: Session, chatroom_id: int) -> Optional[Chatroom]:
@@ -17,3 +19,53 @@ def add_member_to_chatroom(db: Session, chatroom_id: int, member_id: int):
     member = db.query(Member).filter(member_id == Member.id).first()
     chatroom.member.append(member)
     db.commit()
+
+
+def get_private_chatrooms(db: Session, post_id: int, member_id: int) -> List[PrivateChatroom]:
+    post_author_id = db.query(Post.author_id).filter(post_id == Post.id).scalar()
+    is_author = member_id == post_author_id
+
+    query = (
+        db.query(
+            Chatroom.id.label("chatroom_id"),
+            member_chatroom.c.member_id.label("member_id"),
+            Member.nickname.label("member_name"),
+            Chatroom.chat_type.label("chatroom_chat_type"),
+            (member_chatroom.c.member_id == Post.author_id).label("is_teammate")
+        )
+        .join(member_chatroom, Chatroom.id == member_chatroom.c.chatroom_id)
+        .join(Member, Member.id == member_chatroom.c.member_id)
+        .join(Post, Post.id == Chatroom.post_id)
+        .filter(
+            'private' == Chatroom.chat_type,
+            post_id == Chatroom.post_id,
+            member_chatroom.c.member_id != Post.author_id
+        )
+    )
+
+    if not is_author:
+        query = query.filter(member_chatroom.c.member_id == member_id)
+
+    chatrooms_filtered = query.all()
+
+    return [PrivateChatroom(
+        chatroom_id=chatroom.chatroom_id,
+        member_id=chatroom.member_id,
+        member_name=chatroom.member_name,
+        chat_type="private",
+        is_teammate=chatroom.is_teammate,
+    ) for chatroom in chatrooms_filtered]
+
+
+def get_public_chatroom(db: Session, post_id: int, member_id: int) -> PublicChatroom:
+    public_chatroom = db.query(Chatroom).filter_by(post_id=post_id, chat_type="public").first()
+    if public_chatroom is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Public chatroom not found")
+
+    if not is_chatroom_member(db, public_chatroom.id, member_id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Member is not teammate")
+
+    return PublicChatroom(
+        chatroom_id=public_chatroom.id,
+        chat_type="public"
+    )
