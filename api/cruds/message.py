c24b6a3f1467fd3a60d1ec864e8c3bbaafc7b23f
KJH <fighter0628@gmail.com>
2024-07-26 21:32:25 +0900

    feat: 메시지 조회하는 비즈니스 로직 구현
    - get_messages: db에서 메시지를 가져온다
    - 메시지를 가져올 때 페이지네이션을 적용한다
    - message_to_message_log : Message 모델을 message_log 스키마로 변경하는 로직 구현

--- api/cruds/message.py
+++ api/cruds/message.py
@@ -1,9 +1,25 @@
+from typing import Union, Sequence
 from api.models.model import Message
-from api.schemas.message import MessageEvent
+from api.schemas.message import MessageEvent, MessageLog
+from fastapi_pagination.cursor import CursorParams
 from sqlalchemy.orm import Session
+from fastapi_pagination.ext.sqlalchemy import paginate
 
 
 def save_message(db: Session, event: MessageEvent):
     db_message = Message(**event.model_dump())
     db.add(db_message)
     db.commit()
+
+
+def get_messages(db: Session, chatroom_id: int, user_id: int, params: CursorParams):
+    query = db.query(Message).filter(chatroom_id == Message.chatroom_id).order_by(Message.create_at.desc())
+    return paginate(query, params, transformer=lambda messages: message_to_message_log(messages, user_id))
+
+
+def message_to_message_log(messages: Sequence[Message], user_id: int) -> Union[Sequence[MessageLog], None]:
+    return [MessageLog(
+        sender_name=message.member.nickname,
+        is_mine=message.member_id == user_id,
+        contents=message.contents
+    ) for message in messages]
