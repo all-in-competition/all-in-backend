761614892f555a545e6839a3b873f1fa318cc418
KJH <fighter0628@gmail.com>
2024-08-03 00:16:35 +0900

    fix: 스케줄러 실행 조건 수정
    - 자정에 실행되도록 수정

--- api/scheduler.py
+++ api/scheduler.py
@@ -3,4 +3,4 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler(timezone='Asia/Seoul')
 
-scheduler.add_job(update_expired_posts, "cron", day_of_week='mon-sun', hour=0, minute=11, second=30)
+scheduler.add_job(update_expired_posts, "cron", day_of_week='mon-sun', hour=0)
